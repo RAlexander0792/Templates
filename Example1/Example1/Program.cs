4de82465954e1afb08e415fda1b680b008db7edd
Alexander <r.alexander0792@gmail.com>
2018-04-18 19:07:42 -0300

    New Example

---
+++ Example1/Example1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Flia Gordillo
+ * Date: 18/4/2018
+ * Time: 7:06 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Example1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
